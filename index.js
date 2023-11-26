feae55f70d50dee755fc27928e9a7a950124b3ad
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-11-26 20:54:42 +0900

    get all properties from database

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const propertiesCollection = client
+      .db("propertiesDB")
+      .collection("properties");
+    //get all properties
+    app.get("/properties", async (req, res) => {
+      const result = await propertiesCollection.find().toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,7 +47,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
