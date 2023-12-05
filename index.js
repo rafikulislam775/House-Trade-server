83074adfcf57f640dd21206c139c3b0c93e84380
rafikulislam775 <islamrafikul.jp@gmail.com>
2023-12-06 08:29:41 +0900

    users to delete their own reviews

--- index.js
+++ index.js
@@ -166,6 +166,16 @@ async function run() {
       console.log(result);
       res.send(result);
     });
+    //delete properties
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await reviewsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      // console.log(result);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
